8ddeafbb7a59e011f53f710cf4103e0cb382b15d
ramanb <ramanbansal278@gmail.com>
2022-07-27 11:54:06 +0530

    Calculator Operation

--- calc_opreation.c
+++ calc_opreation.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 
-
-// Function for additing two numbers
+// Function for addition for two numbers
 void addition(long int no1,long int no2)
 {
     printf("%ld", no1 + no2);
 }
 
-int main()
+// Function for subtracton of two numbers
+void subtraction(long int no1,long int no2)
+{
+    printf("%ld", no1 - no2);
+}
+
+int main(void)
 {
     // Taking input from user
 
-    printf("Enter number 1 for further operation:\n");
+    printf("Enter number 1 for further operation :\n");
     long int no1;
     scanf("%ld", &no1);
 
     long int no2;
-    printf("Enter number 2 for further operation:\n");
+    printf("Enter number 2 for further operation :\n");
     scanf("%ld", &no2);
 
-    addition(no1,no2);
+// Asking user that which operation he want to perform
 
-    return 0;
+    printf("Enter code of the operation needed:\n 1. Add \t 2. Subtract \n");
+    int check;
+    scanf("%d", &check);
+
+    switch (check)
+    {
+    case 1:
+        addition(no1, no2);
+        break;
+    case 2:
+        subtraction(no1, no2);
+        break;
+    }
 }
