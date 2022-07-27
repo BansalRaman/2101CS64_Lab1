08f1b49c55b1f896d6743e4287ab7ca8b256ba20
Raman Bansal <95139315+BansalRaman@users.noreply.github.com>
2022-07-27 12:17:56 +0530

    Update quick_sort.c

--- quick_sort.c
+++ quick_sort.c
@@ -1,5 +1,3 @@
-// Quick sort in C
-
 #include <stdio.h>
 
 // function to swap elements
@@ -12,22 +10,16 @@ void swap(int *a, int *b) {
 // function to find the partition position
 int partition(int array[], int low, int high) {
   
-  // select the rightmost element as pivot
   int pivot = array[high];
   
-  // pointer for greater element
   int i = (low - 1);
 
   // traverse each element of the array
-  // compare them with the pivot
   for (int j = low; j < high; j++) {
     if (array[j] <= pivot) {
-        
-      // if element smaller than pivot is found
-      // swap it with the greater element pointed by i
+      
       i++;
       
-      // swap element at i with element at j
       swap(&array[i], &array[j]);
     }
   }
@@ -35,16 +27,11 @@ int partition(int array[], int low, int high) {
   // swap the pivot element with the greater element at i
   swap(&array[i + 1], &array[high]);
   
-  // return the partition point
   return (i + 1);
 }
 
 void quickSort(int array[], int low, int high) {
   if (low < high) {
-    
-    // find the pivot element such that
-    // elements smaller than pivot are on left of pivot
-    // elements greater than pivot are on right of pivot
     int pi = partition(array, low, high);
     
     // recursive call on the left of pivot
@@ -63,18 +50,26 @@ void printArray(int array[], int size) {
   printf("\n");
 }
 
-// main function
 int main() {
-  int data[] = {8, 7, 2, 1, 0, 9, 6};
-  
-  int n = sizeof(data) / sizeof(data[0]);
+  // Taking input from user
+
+	printf("Enter Size of array:\n");
+	int n;
+	scanf("%d",&n);
+	printf("Enter Array:\n");
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+
   
   printf("Unsorted Array\n");
-  printArray(data, n);
+  printArray(arr, n);
   
   // perform quicksort on data
-  quickSort(data, 0, n - 1);
+  quickSort(arr, 0, n - 1);
   
   printf("Sorted array in ascending order: \n");
-  printArray(data, n);
+  printArray(arr, n);
 }
